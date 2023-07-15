311df83baedfd7bd9490429729868ee1997a206e
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-07-15 19:24:03 +0300

    error

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,84 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const getContactById = async (contactId) => {}
+// Функція, яка повертає масив контактів.
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const removeContact = async (contactId) => {}
+// Функція, яка повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+const getContactById = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
+    const contactById = allContacts.find((item) => item.id === contactId);
+    if (!contactById) {
+      return null;
+    }
+    return contactById;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const addContact = async (body) => {}
+// Функція, яка повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+const removeContact = async (contactId) => {
+  try {
+    const allContacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+    const index = allContacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = allContacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Функція, яка повертає об'єкт доданого контакту.
+const addContact = async (name, email, phone) => {
+  try {
+    const allContacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Функція, яка повертає оновлений об'єкт доданого контакту.
+const updateContact = async (contactId, body) => {
+  try {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex((el) => el.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    allContacts[index] = { contactId, ...body };
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return allContacts[index];
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +86,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,34 @@ const express = require("express");
 
 const router = express.Router();
 
+const contacts = require("../../models/contacts");
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template test" });
+  const result = await contacts.listContacts();
+  res.json(result);
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.get("/:contactId", async (req, res, next) => {
+//   const {id} = req.params;
+//   const result = await contacts.getContactById(id);
+//   res.json(result);
+// });
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.post("/", async (req, res, next) => {
+//   const result = await contacts.addContact();
+//   res.json(result);
+// });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.delete("/:contactId", async (req, res, next) => {
+//   const {id} = req.params;
+//   const result = await contacts.removeContact(id);
+//   res.json(result);
+// });
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.put("/:contactId", async (req, res, next) => {
+//   const {id} = req.params;
+//   const result = await contacts.updateContact(id);
+//   res.json(result);
+// });
 
 module.exports = router;
