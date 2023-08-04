1fbe9231975ac5751a81c0ac2c9baa1a1c15c32e
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-08-04 05:12:52 +0300

    finished

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const { validateBody } = require("../middlewares/validateBody");
 const { isValidId } = require("../middlewares/isValidId");
 const { validateBodyStatus } = require("../middlewares/validateBodyStatus");
+const { authenticate } = require("./authenticate");
 
 module.exports = {
   validateBody,
   isValidId,
   validateBodyStatus,
+  authenticate,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../../controllers");
-const { validateBody } = require("../../middlewares");
-const { isValidId } = require("../../middlewares");
-const { validateBodyStatus } = require("../../middlewares");
+const ctrl = require("../../controllers/contacts");
+
+const {
+  validateBody,
+  isValidId,
+  validateBodyStatus,
+  authenticate,
+} = require("../../middlewares");
 
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", isValidId, ctrl.remove);
+router.delete("/:contactId", authenticate, isValidId, ctrl.remove);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.update
@@ -24,6 +29,7 @@ router.put(
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateBodyStatus(schemas.updateFavoriteSchema),
   ctrl.updateStatusContact
