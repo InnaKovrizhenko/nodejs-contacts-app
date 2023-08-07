73d8d37f82af83f3d3795befac402c4e4b666ba8
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-08-08 01:55:36 +0300

    finished

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -5,6 +5,7 @@ const { login } = require("./loginUser");
 const { getCurrent } = require("./currentUser");
 const { logout } = require("./logoutUser");
 const { updateSubscription } = require("./updateSubscription");
+const { updateAvatar } = require("./updateAvatar");
 
 module.exports = {
   register: ctrlWrapper(register),
@@ -12,4 +13,5 @@ module.exports = {
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const { validateBody } = require("../middlewares/validateBody");
 const { isValidId } = require("../middlewares/isValidId");
 const { validateBodyStatus } = require("../middlewares/validateBodyStatus");
 const { authenticate } = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   validateBodyStatus,
   authenticate,
+  upload,
 };
